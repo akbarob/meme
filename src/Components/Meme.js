8e5aa086a2716200e5dbbc10903412b94b791883
Akbar <akbarbadmus07@gmail.com>
2022-05-23 11:48:16 +0100

    meme generator done

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,5 +1,5 @@
-import MemeData from "./MemeData"
-import { useState } from "react"
+
+import { useEffect, useState } from "react"
 export default function Meme(){
     const [meme,setMeme] = useState({
         topText: "",
@@ -7,12 +7,18 @@ export default function Meme(){
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeImage,setallMemeImage] =useState(MemeData)
+    const [allMemes, setAllMemes] =useState([])
+
+    useEffect(() => {
+        fetch(`https://api.imgflip.com/get_memes`)
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, [])
 
     function getMemeImage(){
-        const memesArray = MemeData.data.memes
-        const randomNumber = Math.floor(Math.random()* memesArray.length) 
-        const url =memesArray[randomNumber].url
+        
+        const randomNumber = Math.floor(Math.random()* allMemes.length) 
+        const url =allMemes[randomNumber].url
         setMeme(prevstate => ({
             ...prevstate,
             randomImage: url
